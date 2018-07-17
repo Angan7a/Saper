315215ebc00f159d1fea8d4894a05d7b1f462f1f
Andrzej <ganczarek.a@gmil.com>
2018-07-17 22:31:52 +0200

    Fixed bug with incrising to many bombs

--- bombMarker.cpp
+++ bombMarker.cpp
@@ -4,7 +4,7 @@ void bombMarker(int &x, int &y, int &bombs, char tabVisible[10][10]) {
   if (tabVisible[y][x] == '#') {
     bombs--;
     tabVisible[y][x] = '@';
-  } else {
+  } else if (tabVisible[y][x] == '@') {
     tabVisible[y][x] = '#';
     bombs++;
   }
