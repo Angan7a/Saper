12e45ad5c0d7172c95090c8d459addc2404b13bf
Andrzej <ganczarek.a@gmil.com>
2018-07-21 08:24:35 +0200

    Changed message to english and order, hard = 40x40, deleted double enter

--- gameOptions.cpp
+++ gameOptions.cpp
@@ -5,44 +5,42 @@
 #include <unistd.h>
 
 void gameOptions(int &size_x, int &size_y, int &bombs) {
-  std::string options[4] = {"Podaj poziom trudności", "Hard", "Medium", "Easy"};
+  std::string options[4] = {"Level:", "Easy", "Medium", "Hard"};
   system("clear");
   int dificulty;
   dificulty = optionSelect(options);
   switch (dificulty) {
   case 1:
-    size_x = 50;
-    size_y = 50;
-    break;
-  case 2:
-    size_x = 30;
-    size_y = 30;
-    break;
-  case 3:
     size_x = 10;
     size_y = 10;
     break;
+  case 2:
+    size_x = 20;
+    size_y = 20;
+    break;
+  case 3:
+    size_x = 40;
+    size_y = 40;
+    break;
   }
   std::cin.clear();
-  std::cin.ignore(10, '\n');
   system("clear");
   int bombs_number;
-  options[0] = "Wybierz ilość bomb";
-  std::cout << "Wybierz ilość bomb:\n";
+  options[0] = "Number of bombs:";
   if (dificulty == 1) {
-    options[1] = "25";
-    options[2] = "50";
-    options[3] = "100";
+    options[1] = "5";
+    options[2] = "10";
+    options[3] = "20";
     bombs_number = optionSelect(options);
     switch (bombs_number) {
     case 1:
-      bombs = 25;
-      break;
-    case 3:
-      bombs = 100;
+      bombs = 5;
       break;
     case 2:
-      bombs = 50;
+      bombs = 20;
+      break;
+    case 3:
+      bombs = 20;
       break;
     }
 
@@ -65,25 +63,23 @@ void gameOptions(int &size_x, int &size_y, int &bombs) {
     }
 
   } else {
-    options[1] = "5";
-    options[2] = "10";
-    options[3] = "20";
+    options[1] = "25";
+    options[2] = "50";
+    options[3] = "100";
     bombs_number = optionSelect(options);
 
     switch (bombs_number) {
     case 1:
-      bombs = 5;
+      bombs = 25;
       break;
     case 3:
-      bombs = 20;
+      bombs = 50;
       break;
     case 2:
-      bombs = 10;
+      bombs = 100;
       break;
     }
   }
-
-  std::cin.ignore(10, '\n');
 }
 
 int optionSelect(std::string options[4]) {
